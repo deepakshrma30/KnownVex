923f5638e613b1c3d4f5d64525a2dd08623668f8
deepakshrma30 <deepakrosebball1@gmail.com>
2024-10-02 03:44:59 +0530

    mutation

--- lib/axiosInstance.ts
+++ lib/axiosInstance.ts
@@ -1,13 +1,13 @@
-// src/axiosInstance.ts
+
 import axios from 'axios';
 
-// Create an axios instance with a base URL
+
 const axiosInstance = axios.create({
-  baseURL: 'http://3.91.144.249:9000/knowvex/user/', // Replace with your API base URL
+  baseURL: 'http://3.91.144.249:9000/knowvex/user/', 
   headers: {
     'Content-Type': 'application/json',
   },
-  timeout: 5000, // Optional: You can set a timeout for the requests
+  
 });
 
 export default axiosInstance;
